244d51cc9e31d2bde43c6c75d2dffd5958451ea7
John <99531640+John-thabuks@users.noreply.github.com>
2022-06-08 19:09:39 +0300

    👩‍💻 Task 0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """
-    prints all integers in a list
-    Args:
-        my_list: list of integers
-    """
-
-    for integer in my_list:
-        print("{:d}".format(integer))
+def square_matrix_simple(matrix=[]):
+    return [[i ** 2 for i in row] for row in matrix]
