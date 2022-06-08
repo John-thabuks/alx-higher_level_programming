3d00de95d75bfc140035f26ce66f57cde6dfb565
John <99531640+John-thabuks@users.noreply.github.com>
2022-06-08 22:49:48 +0300

    👩‍💻 Task correction

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-    scores = [score*weight for (score, weight) in my_list]
-    return sum(scores) / sum([weight for (score, weight) in my_list])
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
