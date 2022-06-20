b81ad0a82f38130de3a027fd8fe40c0a1ed9ef8d
John <99531640+John-thabuks@users.noreply.github.com>
2022-06-20 23:24:04 +0300

    Task 1 correction

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """
-    Prints an integer with string format for integer
-    Args:
-        value: any data type
-    Return:
-        True if integer else false
-    """
     try:
         print("{:d}".format(value))
     except:
